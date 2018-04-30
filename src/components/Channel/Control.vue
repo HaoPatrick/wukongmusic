548bcb848a727d2555319a604967226cf0d489f8
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2018-04-30 15:41:00 +0800

    :lipstick: ui work on playlist

--- src/components/Channel/Control.vue
+++ src/components/Channel/Control.vue
@@ -21,12 +21,21 @@
     <div v-popover:popover-songs>
       <i class="fab fa-elementor"></i>
     </div>
-    <el-popover ref="popover-songs" placement="left" width="200" trigger="click">
-      <h3>PLAYLIST
+    <el-popover ref="popover-songs" placement="left" width="300" trigger="click">
+      <h3>PLAYLIST ({{songList.songs.length}})
         <i @click="syncMusic" class="fas fa-sync-alt"></i>
+        <i @click="unimplemented" class="fas fa-random"></i>
       </h3>
-      <div>
-        <section v-for='(item,index) in songList' :key="`song-${index}`">{{index}}.{{item}}</section>
+      <div class="play-container">
+        <article v-for='(item,index) in songList.songs' :key="`song-${index}`">
+          <div>{{item.title}} -
+            <span class="artist">{{item.artist}}</span>
+          </div>
+          <div>
+            <i @click="unimplemented" class="fas fa-arrow-up"></i>
+            <i @click="unimplemented" class="far fa-trash-alt"></i>
+          </div>
+        </article>
       </div>
     </el-popover>
 
@@ -83,6 +92,12 @@ export default {
     },
     syncMusic() {
       store.dispatch('fetchSongList', this.userConfig)
+    },
+    unimplemented() {
+      this.$notify({
+        title: 'error',
+        message: 'under construction'
+      })
     }
   }
 }
@@ -101,5 +116,29 @@ export default {
   min-width: 1.5em;
   display: flex;
   justify-content: center;
+}
+.play-container {
+  max-height: 20em;
+  overflow: auto;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  margin-top: 0.2em;
+}
+.play-container article {
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 0.5em 0 0.5em 0;
+  display: flex;
+}
+.play-container article div:nth-of-type(1) {
+  flex-grow: 1;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.play-container article div:nth-of-type(2) {
+  min-width: 2.5em;
+}
+.artist {
+  font-size: 0.7em;
+  color: #999;
 }
 </style>
